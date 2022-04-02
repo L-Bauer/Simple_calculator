c49433114b1694d640d918da182867d1d52ee236
lucas <lucassoccer95@gmai.com>
2022-04-01 20:45:56 -0500

    Creating variables for UI elements

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.simplecalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final EditText firstNumber = findViewById(R.id.number1);
+        final EditText secondNumber = findViewById(R.id.number2);
+        final RadioGroup operators = findViewById(R.id.operators);
+        final RadioButton add = findViewById(R.id.add);
+        final RadioButton subtract = findViewById(R.id.subtract);
+        final Button equals = findViewById(R.id.equals);
+        final TextView result = findViewById(R.id.result);
+
     }
 }
